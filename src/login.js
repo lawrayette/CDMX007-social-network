ba2afecff9ca60a58ce8448e79f76e65297071ae
jaurinu <jaurinu@gmail.com>
2019-03-07 11:24:22 -0600

    Se redirecciona el arhivo logo.png del archivo user.html

--- src/login.js
+++ src/login.js
@@ -17,7 +17,7 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
         },
         // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
         signInFlow: 'popup',
-        signInSuccessUrl: 'main.html',
+        signInSuccessUrl: 'user.html',
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -28,7 +28,7 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
           //firebase.auth.PhoneAuthProvider.PROVIDER_ID
         ],
         // Terms of service url.
-        tosUrl: 'main.html',
+        tosUrl: 'user.html',
         // Privacy policy url.
         //privacyPolicyUrl: '<your-privacy-policy-url>'
       };
