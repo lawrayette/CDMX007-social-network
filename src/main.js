062b9b05fb3b9510a9576ae96716ff336142f802
lawrayette <milhadasdepapel@gmail.com>
2019-03-13 11:07:32 -0600

    cambios en main.js/ agregando imagen/ Aún no funciona x2

--- src/main.js
+++ src/main.js
@@ -123,8 +123,4 @@ userProfile.addEventListener("click", () => {
 
 // 
 // Get a reference to the storage service, which is used to create references in your storage bucket
-var storage = firebase.storage();
 
-// Create a storage reference from our storage service
-var storageRef = storage.ref();
-var imagesRef = storageRef.child('images');
