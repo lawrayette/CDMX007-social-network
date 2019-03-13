19788e5f44fdb32d392c7ef5b424cc7dbcdc1d28
Karen Molina <milhadasdepapel@gmail.com>
2019-03-13 11:26:32 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -52,19 +52,19 @@ db.collection("state").onSnapshot((querySnapshot) => {
   querySnapshot.forEach((doc) => {
     console.log(`${doc.id} => ${doc.data().first}`);
     table.innerHTML += `
-
-    <div>
+    <div class="card  text-center alert alert-info">
        <p>${doc.data().name}</p>
-      <td>${doc.data().first}</td>
+      <p>${doc.data().first}</p>
+      <p>${doc.data().image}</p>
       <li>${doc.data().area}</li>
-      <div id="applause-container"><applause-button id="applause-${doc.id}" url="http://localhost:8887/${doc.id}" multiclap="true" class="applause-clase" color="Black"/></div>
-
       <p>
-      <button class = "btn btn-danger" onclick = "deleteData('${doc.id}')"> Eliminar </button>
-      <button class = "btn btn-warning" onclick = "editState('${doc.id}','${doc.data().first}','${doc.data().name}','${doc.data().area}')"> Editar </button>
+      <button class = "btn btn-danger btn-sm" onclick = "deleteData('${doc.id}')"><i class="fas fa-trash-alt"></i></button>
+      <button class = "btn btn-warning btn-sm" onclick = "editState('${doc.id}','${doc.data().first}','${doc.data().name}','${doc.data().area}')"><i class="fas fa-edit"></i></button>
+     <a href="https://twitter.com/share?url=https://jaurinu.github.io/CDMX007-social-network/src/&amp;text=Punto%20STEAM%20&amp;hashtags=puntosteam" target="_blank">
+     <img src="https://simplesharebuttons.com/images/somacro/twitter.png" width="25 height="25" alt="Twitter" /></a>
+     <button id="applause-container"><applause-button id="applause-${doc.id}" url="http://localhost:8887/${doc.id}" multiclap="true" class="applause-clase" color="Black"/></button>
      </p>
-
-     </div>
+    </div>
     `
   });
 });
@@ -83,7 +83,7 @@ function deleteData(id) {
 }
 
 //Edita los datos
-function editState(id, state,name) {
+function editState(id, state, name) {
   document.getElementById('input').value = state;
   document.getElementById('name-input').value = name;
 
@@ -111,7 +111,6 @@ function editState(id, state,name) {
       });
   }
 }
-
 
 //See User
 const userProfile = document.getElementById('button-user')
