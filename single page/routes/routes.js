6a6c82d438b63f285c9cd8548156a17254cae427
AndreaMyshkn <atrkvsk@hotmail.com>
2019-03-19 08:52:54 -0600

    ...

--- single page/routes/routes.js
+++ single page/routes/routes.js
@@ -1,6 +1,6 @@
 (function (window, document){
 library.getID('view').enroute()
-.route ('/', 'views/login.html', null, null )
+.route ('/', './views/login.html', null, null )
 .route ('/forum', 'views/forum.html', null, null)
  //hideLoadingshowMenu()//)//En lugar de null podrías implementar una función para que se ejecute inmediatamente al cargar la página)
 .route ('/profile', 'views/profile.html', null, null)
