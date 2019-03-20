591da53a60515821652e5351fee25ff35da1b85d
JazminDominguez <j.dgez.p@gmail.com>
2019-03-20 09:02:17 -0600

    preparativos para el SPA

--- src/js/controlador.js
+++ src/js/controlador.js
@@ -19,7 +19,7 @@ libreria.controlador('miControlador', {
                 },
                 // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
                 signInFlow: 'popup',
-                signInSuccessUrl: 'user.html',
+                signInSuccessUrl: '03user.html',
                 signInOptions: [
                   // Leave the lines as is for the providers you want to offer your users.
                   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
