a0398517fe2cb1faf753ac6a454b242808fce5ca
jaurinu <jaurinu@gmail.com>
2019-03-20 13:17:02 -0600

    Mudanza de materialize en archivo forum

--- src/forum1.js
+++ src/forum1.js
@@ -17,3 +17,19 @@
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems);
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left',
+      hoverEnabled: false
+    });
+  });
+
+  alert('hgcuif')
